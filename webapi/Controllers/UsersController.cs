19686f1bd98b003b9bef5c6a61f61a77ed8fe64a
Vladwoguer Bezerra <vladwoguerbezerra@gmail.com>
2019-01-09 01:26:30 -0300

    first commit

---
+++ webapi/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    public class UsersController : ApiController
+    {
+
+        private User[] users = new User[]
+        {
+            new User { id = 1, name = "Vladwoguer Bezerra", email = "vladwoguerbezerra@gmail.com", phone = "01111111", role = 1},
+            new User { id = 2, name = "Tacila Magalhães", email = "vladwoguerbezerra@gmail.com", phone = "01111111", role = 1},
+        };
+
+
+        /// <summary>
+        /// Gets the users from the server.
+        /// </summary>
+        // GET: api/Users
+        [ResponseType(typeof(IEnumerable<User>))]
+        public IEnumerable<User> Get()
+        {
+            return users;
+        }
+
+        /// <summary>
+        /// Looks up some user by ID.
+        /// </summary>
+        /// <param name="id">The ID of the user.</param>
+        // GET: api/Users/5
+        public IHttpActionResult Get(int id)
+        {
+            var product = users.FirstOrDefault((p) => p.id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ webapi/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        /// <summary>
+        /// Gets the values from the server.
+        /// </summary>
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Looks up some value by ID.
+        /// </summary>
+        /// <param name="id">The ID of the value.</param>
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Insert new value on the server.
+        /// </summary>
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Update value by ID.
+        /// </summary>
+        /// <param name="id">The ID of the value.</param>
+        /// <param name="value">The value.</param>
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Delete value by ID.
+        /// </summary>
+        /// <param name="id">The ID of the value.</param>
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
